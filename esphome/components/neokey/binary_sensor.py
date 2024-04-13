7b5ab325ce1cdfbd66634a8c508f260e027a1548
Daniel Eisterhold <deisterhold@gmail.com>
2024-04-13 04:09:10 -0500

    Add wrapper component for Adafruit NeoKey.

---
+++ esphome/components/neokey/binary_sensor.py
@@ -0,0 +1,30 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import binary_sensor
+from esphome.const import CONF_ADDRESS
+
+CODEOWNERS = ["@deisterhold"]
+
+DEPENDENCIES = ["i2c"]
+
+neokey_ns = cg.esphome_ns.namespace("neokey")
+
+NeoKeyBinarySensor = neokey_ns.class_(
+    "NeoKeyBinarySensor", binary_sensor.BinarySensor, cg.Component
+)
+
+CONFIG_SCHEMA = (
+    binary_sensor.binary_sensor_schema(NeoKeyBinarySensor)
+    .extend({cv.Optional(CONF_ADDRESS, default=0x30): cv.i2c_address})
+    .extend(cv.COMPONENT_SCHEMA)
+)
+
+
+async def to_code(config):
+    var = await binary_sensor.new_binary_sensor(config)
+    await cg.register_component(var, config)
+
+    cg.add(var.set_address(config[CONF_ADDRESS]))
+
+    cg.add_library("adafruit/Adafruit BusIO")
+    cg.add_library("adafruit/Adafruit seesaw Library")
