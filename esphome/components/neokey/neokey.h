10d16f7fefbca1d0750f3403f0a2011d2a992615
Daniel Eisterhold <deisterhold@gmail.com>
2024-04-18 14:41:31 -0500

    Changes.

--- esphome/components/neokey/neokey.h
+++ esphome/components/neokey/neokey.h
@@ -2,6 +2,8 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/log.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/light/addressable_light.h"
 #include "esphome/components/i2c/i2c.h"
 
 #include "Adafruit_NeoKey_1x4.h"
@@ -10,12 +12,50 @@
 namespace esphome {
 namespace neokey {
 
+#define NUM_BYTES_PER_LED 3
+
+static const char *const TAG = "neokey";
+
 class KeyListener {
  public:
   virtual void keys_update(uint8_t keys){};
 };
 
-static const char *const TAG = "neokey";
+class NeoKeyBinarySensor : public binary_sensor::BinarySensor, public KeyListener {
+ public:
+  void set_key(uint8_t key) { key_ = key; };
+  void keys_update(uint8_t keys) override;
+ protected:
+  uint8_t key_{0};
+};
+
+class NeoKeyLight : public light::AddressableLight {
+ public:
+  void setup() override;
+  void set_neokey(NeoKeyComponent *neokey) { neokey_ = neokey; }
+  int32_t size() const override { return this->neokey_ == nullptr ? 0 : this->neokey_->hub_.pixels.numPixels(); }
+  void write_state(light::LightState *state) override;
+  void clear_effect_data() override {
+    for (int i = 0; i < this->size(); i++)
+      this->effect_data_[i] = 0;
+  }
+  light::LightTraits get_traits() override {
+    auto traits = light::LightTraits();
+    traits.set_supported_color_modes({light::ColorMode::RGB});
+    return traits;
+  }
+ protected:
+  NeoKeyComponent *neokey_;
+
+  uint8_t *buf_{nullptr};
+  uint8_t *effect_data_{nullptr};
+
+  light::ESPColorView get_view_internal(int32_t index) const override {
+    uint8_t *base = this->buf_ + (NUM_BYTES_PER_LED * index);
+
+    return light::ESPColorView(base + 0, base + 1, base + 2, nullptr, this->effect_data_ + index, &this->correction_);
+  }
+};
 
 class NeoKeyComponent : public PollingComponent, public i2c::I2CDevice {
  public:
@@ -28,6 +68,7 @@ class NeoKeyComponent : public PollingComponent, public i2c::I2CDevice {
   void register_listener(KeyListener *listener) { this->listeners_.push_back(listener); }
 
  protected:
+  friend NeoKeyLight;
   Adafruit_NeoKey_1x4 hub_;
   std::vector<KeyListener *> listeners_{};
 };
