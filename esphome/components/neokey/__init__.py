b2818c32f1de3e5151d8c781c9938106e0549de6
Daniel Eisterhold <deisterhold@gmail.com>
2024-04-18 11:36:45 -0500

    Remove Arduino framework validation.

--- esphome/components/neokey/__init__.py
+++ esphome/components/neokey/__init__.py
@@ -23,15 +23,14 @@ CONFIG_SCHEMA = (
         }
     )
     .extend(cv.polling_component_schema("10ms"))
-    .extend(i2c.i2c_device_schema(0x30)),
-    cv.only_with_arduino,
+    .extend(i2c.i2c_device_schema(0x30))
 )
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await i2c.register_i2c_device(var, config)
     await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
 
     cg.add_library("SPI", None)
     cg.add_library("Wire", None)
--- esphome/components/neokey/binary_sensor/__init__.py
+++ esphome/components/neokey/binary_sensor/__init__.py
@@ -15,7 +15,7 @@ CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(NeoKeyBinarySensor).extend(
 
 
 async def to_code(config):
+    hub = await cg.get_variable(config[CONF_NEOKEY_ID])
     var = await binary_sensor.new_binary_sensor(config)
     cg.add(var.set_key(config[CONF_KEY]))
-    hub = await cg.get_variable(config[CONF_NEOKEY_ID])
     cg.add(hub.register_listener(var))
