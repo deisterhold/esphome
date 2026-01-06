bb8781b4602021a0edff23a5fa378fe345d6e947
Daniel Eisterhold <deisterhold@gmail.com>
2026-01-06 04:16:33 +0000

    Add log helper macro.

--- esphome/components/pn532_uart/pn532_uart.cpp
+++ esphome/components/pn532_uart/pn532_uart.cpp
@@ -1,6 +1,7 @@
 #include "pn532_uart.h"
+#include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
-#include "esphome/core/hal.h"
+// #include "esphome/core/hal.h"
 
 // Based on:
 // - https://cdn-shop.adafruit.com/datasheets/PN532C106_Application+Note_v1.2.pdf
